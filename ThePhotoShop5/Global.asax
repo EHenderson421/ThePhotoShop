b217486a6e0aeaac2fa90ec92419aa510bc69106
ehenderson421 <ehenderson421@gmail.com>
2018-05-17 13:40:17 -0500

    first push

---
+++ ThePhotoShop5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThePhotoShop5.MvcApplication" Language="C#" %>
---
+++ ThePhotoShop5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThePhotoShop5.Startup))]
+namespace ThePhotoShop5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
