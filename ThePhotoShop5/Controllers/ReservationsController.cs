ac2662d998ef2bd28ab18b1e829ed832d05082d9
ehenderson421 <ehenderson421@gmail.com>
2018-05-18 14:20:40 -0500

    add controller and views for reservation

---
+++ ThePhotoShop5/Controllers/ReservationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ThePhotoShop5.Models;
+
+namespace ThePhotoShop5.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Reservations
+        public ActionResult Index()
+        {
+            var reservations = db.Reservations.Include(r => r.Client).Include(r => r.Location);
+            return View(reservations.ToList());
+        }
+
+        // GET: Reservations/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservations/Create
+        public ActionResult Create()
+        {
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName");
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "NameOfLocation");
+            return View();
+        }
+
+        // POST: Reservations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ReservationId,ClientId,LocationId,AppointmentDateTime,AppointmentConfirmation")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reservations.Add(reservation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", reservation.ClientId);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "NameOfLocation", reservation.LocationId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", reservation.ClientId);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "NameOfLocation", reservation.LocationId);
+            return View(reservation);
+        }
+
+        // POST: Reservations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ReservationId,ClientId,LocationId,AppointmentDateTime,AppointmentConfirmation")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ClientId = new SelectList(db.Clients, "ClientId", "FirstName", reservation.ClientId);
+            ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "NameOfLocation", reservation.LocationId);
+            return View(reservation);
+        }
+
+        // GET: Reservations/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Reservation reservation = db.Reservations.Find(id);
+            if (reservation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Reservation reservation = db.Reservations.Find(id);
+            db.Reservations.Remove(reservation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
